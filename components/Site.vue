316bb62d07e96eab3404cb106c463496b1a92c60
Evgeniy Lobanov <dirtha@yandex.ru>
2021-10-13 01:49:05 +0300

    Prepared to deploy

--- components/Site.vue
+++ components/Site.vue
@@ -2,18 +2,14 @@
   <v-container>
     <v-row>
       <v-col>
-        <v-card height="400px">
+        <v-card min-height="400px">
           <v-card-title>Piesocket</v-card-title>
           <iframe
             src="https://www.piesocket.com/websocket-tester"
             frameborder="0"
             width="100%"
-            height="350px"
-            @load="piesocketError = true"
-            @error="piesocketError = true"
-            v-if="!piesocketError"
+            height="400px"
           ></iframe>
-          <p class="text-h5 mt-4 text-center" v-else>Error</p>
         </v-card>
       </v-col>
       <v-col>
@@ -24,7 +20,6 @@
             frameborder="0"
             width="100%"
             height="400px"
-            @error="websocketError = true"
           ></iframe>
         </v-card>
       </v-col>
@@ -33,22 +28,7 @@
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      piesocketError: false,
-      websocketError: false
-    };
-  },
-  methods: {
-    onLoad() {
-      console.log("1yes!");
-    },
-    onError() {
-      console.log($event);
-    }
-  }
-};
+export default {};
 </script>
 
 <style>
